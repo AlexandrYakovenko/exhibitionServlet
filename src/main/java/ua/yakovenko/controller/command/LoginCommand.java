c91a044c12e33890f2f490d0d4dd73b9f1d8ac2b
Alexandr Yakovenko <yakovenko9814@ukr.net>
2019-10-04 15:56:03 +0300

    add Log4j2

--- src/main/java/ua/yakovenko/controller/command/LoginCommand.java
+++ src/main/java/ua/yakovenko/controller/command/LoginCommand.java
@@ -1,5 +1,7 @@
 package ua.yakovenko.controller.command;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.yakovenko.model.entity.Role;
 import ua.yakovenko.model.entity.User;
 import ua.yakovenko.model.service.UserService;
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 public class LoginCommand implements Command {
+    private static final Logger LOGGER = LogManager.getLogger(LoginCommand.class);
     private static final String DATA_ERROR = "Invalid username or password.";
     private static final String LOGGED_ERROR = "You has already logged.";
 
@@ -39,12 +42,21 @@ public class LoginCommand implements Command {
 
         if (user.get().getRole().equals(Role.ADMIN)) {
             CommandUtility.setUser(request, user.get());
+
+            LOGGER.info(user.get() + " logged successfully.");
+
             return "redirect:/exhibition/admin";
         } else if (user.get().getRole().equals(Role.SUPER_ADMIN)) {
             CommandUtility.setUser(request, user.get());
+
+            LOGGER.info(user.get() + " logged successfully.");
+
             return "redirect:/exhibition/super_admin";
         } else {
             CommandUtility.setUser(request, user.get());
+
+            LOGGER.info(user.get() + " logged successfully.");
+
             return "redirect:/exhibition/user";
         }
 
